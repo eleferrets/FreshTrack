04277f381db88d1d13e693c01e8989080e5dd917
Brian Balthazar <40572853+eleferrets@users.noreply.github.com>
2023-02-19 18:32:51 -0500

    Add date import and cleanup redundant code

--- components/DetailsScreen.js
+++ components/DetailsScreen.js
@@ -1,8 +1,6 @@
 import { useState } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button } from 'react-native';
 import {Input} from 'react-native-elements'
-import DatePicker from 'react-native-datepicker';
-
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 export default function DetailsScreen({navigation}) {
@@ -39,41 +37,6 @@ export default function DetailsScreen({navigation}) {
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Input placeholder="Name" value={name} onChangeText={(text) =>setName(text)}/>
       <Input placeholder="Category" value={category} onChangeText={(text) =>setCategory(text)}/>
-      {/* <DatePicker
-          style={styles.datePickerStyle}
-          date={date}
-          mode="date"
-          placeholder="select date"
-          format="DD/MM/YYYY"
-          minDate="01-01-2023"
-          maxDate="01-01-2040"
-          confirmBtnText="Confirm"
-          cancelBtnText="Cancel"
-          customStyles={{
-            dateIcon: {
-              position: 'absolute',
-              right: -5,
-              top: 4,
-              marginLeft: 0,
-            },
-            dateInput: {
-              borderColor : "gray",
-              alignItems: "flex-start",
-              borderWidth: 0,
-              borderBottomWidth: 1,
-            },
-            placeholderText: {
-              fontSize: 17,
-              color: "gray"
-            },
-            dateText: {
-              fontSize: 17,
-            }
-          }}
-          onDateChange={(date) => {
-            setDate(date);
-          }}
-        /> */}
         <Button title="Show Dates" onPress={showDatePicker} />
       <DateTimePickerModal
         isVisible={isDatePickerVisible}
