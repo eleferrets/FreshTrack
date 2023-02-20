a178583851c4311e8fb35b9996d265c53d81f41d
Brian Balthazar <40572853+eleferrets@users.noreply.github.com>
2023-02-19 22:26:06 -0500

    Show entries or not if there is data on homescreen

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,9 +1,35 @@
-import { Text, View, Button } from 'react-native';
+import { Text, View, Button, StyleSheet } from 'react-native';
 
-export default function HomeScreen({ navigation }) {
+export default function HomeScreen({ route, navigation }) {
+    const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          padding: 50,
+        },
+        item: {
+          padding: 20,
+          fontSize: 15,
+          marginTop: 5,
+        }
+      });
+      const data = [];
+      if (route.params) {
+        // const {name} = route.params;
+        // console.log(route.params)
+        data.push(route.params);
+      }
+
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Nothing here yet!</Text>
+        { data.length == 0 && <Text>Nothing here yet!</Text>}
+        {data.length > 0 && data.map((person) => {
+        return (
+          <View>
+            <Text style={styles.item} key={person.name}>{person.name}</Text>
+          </View>
+        );
+      })}
+      
         <Button
           title="Go to Details"
           onPress={() => navigation.navigate('Details')}
