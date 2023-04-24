166f68763c8a0aeb617ca4768c91e1506fa6e185
mflynn19 <meghan.flynn@theflynn.org>
2023-04-24 17:04:41 -0400

    tab nav good except for new item

--- App.js
+++ App.js
@@ -5,18 +5,29 @@ import DetailsScreen from './components/DetailsScreen'
 import HomeScreen from './components/HomeScreen';
 import BarcodeScanner from './components/BarcodeScanner';
 import EditScreen from "./components/EditScreen";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Stack = createNativeStackNavigator();
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Edit" component={EditScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Home' }} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-        <Stack.Screen name="Barcode" component={BarcodeScanner} />
-        <Stack.Screen name="Edit" component={EditScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen name="Home" component={HomeStackScreen} options={{ title: 'Home' }} />
+        <Tab.Screen name="Add New Item" component={DetailsScreen} />
+        <Tab.Screen name="Scan Barcode" component={BarcodeScanner} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
