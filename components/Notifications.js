206e6c713f51b3222eac1b29ca869fd7bd2a0e4c
Brian Balthazar <40572853+eleferrets@users.noreply.github.com>
2023-04-24 13:04:12 -0400

    The base notifications object It has creation, update, and delete using ids and they go off at 12

---
+++ components/Notifications.js
@@ -0,0 +1,88 @@
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+let nextId = 1;
+
+const getNotificationId = () => {
+  const id = nextId;
+  nextId += 1;
+  return id.toString();
+};
+
+export const scheduleNotification = async (title, body, date) => {
+  const trigger = new Date(date);
+  trigger.setHours(12);
+  trigger.setMinutes(0);
+  trigger.setSeconds(0);
+
+  const id = getNotificationId();
+
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: title,
+      body: body,
+      data: { id },
+    },
+    trigger: {
+      year: trigger.getFullYear(),
+      month: trigger.getMonth(),
+      day: trigger.getDate(),
+      hour: trigger.getHours(),
+      minute: trigger.getMinutes(),
+    },
+  });
+
+  return id;
+};
+
+export async function scheduleNotificationInstant(title, body) {
+    const trigger = new Date(Date.now() + 60000); // Schedule the notification 1 minute in the future
+    const notificationId = await Notifications.scheduleNotificationAsync({
+      content: {
+        title: title,
+        body: body,
+      },
+      trigger: trigger,
+    });
+    return notificationId;
+  }
+
+export const updateNotification = async (id, title, body, date) => {
+  const trigger = new Date(date);
+  trigger.setHours(12);
+  trigger.setMinutes(0);
+  trigger.setSeconds(0);
+
+  const notificationId = id.toString();
+
+  await Notifications.cancelScheduledNotificationAsync(notificationId);
+
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: title,
+      body: body,
+      data: { id },
+    },
+    trigger: {
+      year: trigger.getFullYear(),
+      month: trigger.getMonth(),
+      day: trigger.getDate(),
+      hour: trigger.getHours(),
+      minute: trigger.getMinutes(),
+    },
+  });
+
+  return notificationId;
+};
+
+export const cancelNotification = async (id) => {
+  const notificationId = id.toString();
+  await Notifications.cancelScheduledNotificationAsync(notificationId);
+};
