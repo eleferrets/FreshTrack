283872cddf1b69421bff0c67ba210f84d806fab7
mflynn19 <meghan.flynn@theflynn.org>
2023-04-24 17:17:59 -0400

    aadded icons and focus color

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import HomeScreen from './components/HomeScreen';
 import BarcodeScanner from './components/BarcodeScanner';
 import EditScreen from "./components/EditScreen";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const HomeStack = createNativeStackNavigator();
 
@@ -23,9 +24,29 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name="Home" component={HomeStackScreen} options={{ title: 'Home' }} />
+      <Tab.Navigator initialRouteName="Home" screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home-outline'
+                : 'home-outline';
+            } else if (route.name === 'Add New Item') {
+              iconName = focused ? 'add-circle-outline' : 'add-circle-outline';
+            }
+            else if (route.name === 'Scan Barcode') {
+              iconName = focused ? 'scan-circle-outline' : 'scan-circle-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'green',
+          tabBarInactiveTintColor: 'gray',
+        })}>
         <Tab.Screen name="Add New Item" component={DetailsScreen} />
+        <Tab.Screen name="Home" component={HomeStackScreen} options={{ title: 'Home' }} />
         <Tab.Screen name="Scan Barcode" component={BarcodeScanner} />
       </Tab.Navigator>
     </NavigationContainer>
